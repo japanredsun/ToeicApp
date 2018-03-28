76c955e0e763eb01bff2c537567a9fac30ede6bc
Japanredsun <japanredsun@gmail.com>
2018-03-28 14:34:09 +0700

    update types

--- src/main/java/com/japanredsun/Service/Service.java
+++ src/main/java/com/japanredsun/Service/Service.java
@@ -11,8 +11,13 @@ public class Service {
         return FXCollections.observableArrayList(active,inactive);
     }
     public ObservableList<String> getTypes(){
-        String type1 = "Listening";
-        String type2 = "Reading";
-        return FXCollections.observableArrayList(type1,type2);
+        String type1 = "Listening - Part 1";
+        String type2 = "Listening - Part 2";
+        String type3 = "Listening - Part 3";
+        String type4 = "Listening - Part 4";
+        String type5 = "Reading - Part 5";
+        String type6 = "Reading - Part 6";
+        String type7 = "Reading - Part 7";
+        return FXCollections.observableArrayList(type1,type2,type3,type4,type5,type6,type7);
     }
 }
